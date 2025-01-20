9f36ec5c8e15297465433005d8f8b71ede6a56fe
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2025-01-20 22:48:47 +0200

    fixed dts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 export { PlaywrightWorld } from './src/PlaywrightWorld';
 export { defineCucumber } from './src/defineCucumber';
-export { defineCucumber } from './src/tags';
+export { tags } from './src/tags';
