31c73abb66a559c747a1cc1159d4387c880279e8
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2025-10-03 13:14:05 +0300

    improved Template types and docs

--- src/Template.ts
+++ src/Template.ts
@@ -1,7 +1,16 @@
+/**
+ * Define template step
+ * @param {string} scenario - multiline string with steps
+ * @example
+ * When('I click {string} and verify {string}', Template((locator, expected) => `
+ *     I click '${locator}'
+ *     I expect '${locator} > Value' to equal '${expected}'
+ * `));
+ */
 export function Template(scenario: (...args: any[]) => string) {
     return new Proxy(scenario, {
-        apply: async function (template, world, args) {
-            const scenario = template(...args) as string;
+        apply: async function (template: (...args: string[]) => string, world: { executeStep: (step: string) => Promise<void> }, args: any[]) {
+            const scenario = template(...args) ;
             const steps = scenario
                 .split('\n')
                 .map(step => step.trim())
